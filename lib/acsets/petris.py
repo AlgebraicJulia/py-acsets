1a233cc88bc9c7bb79d687e74a74905223ac6123
mehalter <mehalter@users.noreply.github.com>
2023-04-11 12:52:36 +0000

    deploy: 552c296f0c9998c21054b71705c018364c151489

--- lib/acsets/petris.py
+++ lib/acsets/petris.py
@@ -2,7 +2,19 @@
 In this model, we define a schema for petri nets, and then a subclass of acset
 with some convenience methods.
 """
-from acsets import SCHEMAS_DIRECTORY, ACSet, Attr, AttrType, Hom, Ob, Schema
+from pathlib import Path
+
+from acsets import (
+    CATLAB_SCHEMAS_DIRECTORY,
+    HERE,
+    JSON_SCHEMAS_DIRECTORY,
+    ACSet,
+    Attr,
+    AttrType,
+    Hom,
+    Ob,
+    Schema,
+)
 
 Species = Ob(name="S", title="Species")
 Transition = Ob(name="T", title="Transition")
@@ -15,6 +27,9 @@ hom_ot = Hom(name="ot", dom=Output, codom=Transition, title="Output transition m
 hom_os = Hom(name="os", dom=Output, codom=Species, title="Output species morphism")
 
 Name = AttrType(name="Name", ty=str, title="Name")
+Concentration = AttrType(name="Concentration", ty=int, title="Concentration")
+Rate = AttrType(name="Rate", ty=float, title="Rate")
+Prop = AttrType(name="Prop", ty=dict, title="Property")
 
 attr_sname = Attr(
     name="sname",
@@ -23,6 +38,20 @@ attr_sname = Attr(
     title="Species name",
     description="An attribute representing the name of a species.",
 )
+attr_concentration = Attr(
+    name="concentration",
+    dom=Species,
+    codom=Concentration,
+    title="Species concentration",
+    description="An attribute representing the concentration of a species.",
+)
+attr_sprop = Attr(
+    name="sprop",
+    dom=Species,
+    codom=Prop,
+    title="Species properties",
+    description="An attribute representing the properties of a species.",
+)
 attr_tname = Attr(
     name="tname",
     dom=Transition,
@@ -30,13 +59,76 @@ attr_tname = Attr(
     title="Transition name",
     description="An attribute representing the name of a transition.",
 )
+attr_rate = Attr(
+    name="rate",
+    dom=Transition,
+    codom=Rate,
+    title="Transition rate",
+    description="An attribute representing the rate of a transition.",
+)
+attr_tprop = Attr(
+    name="tprop",
+    dom=Transition,
+    codom=Prop,
+    title="Transition properties",
+    description="An attribute representing the properties of a transition.",
+)
 
-SchPetri = Schema(
-    "Petri",
+SchPetriNet = Schema(
+    "PetriNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [],
+    [],
+)
+SchLabelledPetriNet = Schema(
+    "LabelledPetriNet",
     [Species, Transition, Input, Output],
     [hom_it, hom_is, hom_ot, hom_os],
     [Name],
     [attr_sname, attr_tname],
+)
+SchReactionNet = Schema(
+    "ReactionNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Concentration, Rate],
+    [attr_concentration, attr_rate],
+)
+SchLabelledReactionNet = Schema(
+    "LabelledReactionNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Name, Concentration, Rate],
+    [attr_sname, attr_concentration, attr_tname, attr_rate],
+)
+SchPropertyPetriNet = Schema(
+    "PropertyPetriNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Prop],
+    [attr_sprop, attr_tprop],
+)
+SchPropertyLabelledPetriNet = Schema(
+    "PropertyLabelledPetriNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Name, Prop],
+    [attr_sname, attr_sprop, attr_tname, attr_tprop],
+)
+SchPropertyReactionNet = Schema(
+    "PropertyReactionNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Concentration, Rate, Prop],
+    [attr_concentration, attr_sprop, attr_rate, attr_tprop],
+)
+SchPropertyLabelledReactionNet = Schema(
+    "PropertyLabelledReactionNet",
+    [Species, Transition, Input, Output],
+    [hom_it, hom_is, hom_ot, hom_os],
+    [Name, Concentration, Rate, Prop],
+    [attr_sname, attr_concentration, attr_sprop, attr_tname, attr_rate, attr_tprop],
 )
 
 
@@ -49,13 +141,15 @@ class Petri(ACSet):
         sir = Petri()
         s,i,r = sir.add_species(3)
         sir.set_subpart(s,attr_sname,"susceptible")
+        sir.set_subpart(s,attr_sprop, { "uuid": "dae22e85-d941-4156-b559-d153a44356f3" })
         inf = sir.add_transitions([([s,i],[i,i])])
-        sir.set_subpart(inf,attr_tname,"infection")  sir = Petri()
+        sir.set_subpart(inf,attr_tname,"infection")
+        sir.set_subpart(inf,attr_tprop, { "uuid": "bba26d0e-3ce5-41e5-ac0e-6be35535d534" })
     """
 
-    def __init__(self, name="Petri", schema=SchPetri):
+    def __init__(self, name="Petri", schema=SchPropertyLabelledReactionNet):
         """Initialize a new petri net."""
-        super(Petri, self).__init__(name, schema)
+        super(Petri, self).__init__(schema.name, schema)
 
     def add_species(self, n: int) -> range:
         """Add `n` number of species to the petri net
@@ -91,7 +185,24 @@ class Petri(ACSet):
 
 
 if __name__ == "__main__":
-    SchPetri.write_schema(
-        SCHEMAS_DIRECTORY.joinpath("petri.json"),
-        uri="https://raw.githubusercontent.com/AlgebraicJulia/py-acsets/main/src/acsets/schemas/petri.json",
-    )
+    for schema in [
+        SchPetriNet,
+        SchLabelledPetriNet,
+        SchReactionNet,
+        SchLabelledReactionNet,
+        SchPropertyPetriNet,
+        SchPropertyLabelledPetriNet,
+        SchPropertyReactionNet,
+        SchPropertyLabelledReactionNet,
+    ]:
+        schema_filename = "{}.json".format(schema.name)
+        CATLAB_SCHEMAS_DIRECTORY.joinpath(schema_filename).write_text(
+            schema.schema.json(indent=2, ensure_ascii=False, sort_keys=True)
+        )
+        jsonschema_path = JSON_SCHEMAS_DIRECTORY.joinpath(schema_filename)
+        schema.write_schema(
+            JSON_SCHEMAS_DIRECTORY.joinpath(jsonschema_path),
+            uri="https://raw.githubusercontent.com/AlgebraicJulia/py-acsets/main/src/acsets/{}".format(
+                Path(jsonschema_path).relative_to(HERE)
+            ),
+        )
