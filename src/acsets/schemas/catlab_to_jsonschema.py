8571a09103e1be6dba6c299882b73a1e399d3061
James <fairbanksj@ufl.edu>
2023-09-27 12:05:32 -0400

    Create SummationDecapode.json (#33)
    * Create SummationDecapode.json
    
    * Update SummationDecapode.json
    
    fill in title and description fields
    
    * use ty key in SummationDecapode.json
    
    * Export to jsonschema
    
    * Update setup.cfg
    
    * Update conf.py
    
    * Update conf.py
    
    * Update
    
    ---------
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

---
+++ src/acsets/schemas/catlab_to_jsonschema.py
@@ -0,0 +1,40 @@
+"""This script turns all the catlab schemata into json schemata"""
+
+from pathlib import Path
+from acsets import (
+    CATLAB_SCHEMAS_DIRECTORY,
+    ACSet,
+    Attr,
+    AttrType,
+    CatlabSchema,
+    Hom,
+    Ob,
+    petris,
+    Schema,
+)
+
+HERE = Path(__file__).parent.resolve()
+CATLAB = HERE.joinpath("catlab")
+JSONSCHEMA = HERE.joinpath("jsonschema")
+
+
+def main():
+    """Convert any Catlab schemas into JSON schemas"""
+    for catlab_path in CATLAB.glob("*.json"):
+        jsonschema_path = JSONSCHEMA.joinpath(catlab_path.name)
+        if jsonschema_path.is_file():
+            continue
+        print(f"Parsing Catlab Schema from {catlab_path}")
+        catlab_schema = CatlabSchema.parse_file(catlab_path)
+        print("Getting Schema from Catlab schema")
+        schema = Schema.from_catlab(catlab_path.name, catlab_schema)
+        schema.write_schema(
+            jsonschema_path,
+            uri="https://raw.githubusercontent.com/AlgebraicJulia/py-acsets/main/src/acsets/{}".format(
+                Path(jsonschema_path).relative_to(HERE)
+            ),
+        )
+
+
+if __name__ == "__main__":
+    main()
